3bd1029f624dc11d54a77496004c638a3204c2bc
Axel Paillaud <contact@axelweb.fr>
2025-01-22 17:02:45 +0000

    prompt example

--- compare.js
+++ compare.js
@@ -1,8 +1,7 @@
 import rl from "./eemi-lib.js";
 
-rl.question("Quel est votre nom ? ", function(name) {
-    rl.question("Quel est votre âge ? ", function(age) {
-        console.log(`Bonjour, ${name}. Vous avez ${age} ans.`);
-        rl.close();
-    });
+rl.question("Quel est votre nom ? ", (userName) => {
+    console.log(`Bonjour, ${userName} !`);
+    rl.close();
 });
+
