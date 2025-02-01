88f12359c38f9ccb0036283495f5bbe688ccabd5
Axel Paillaud <contact@axelweb.fr>
2025-02-01 07:04:03 +0100

    init cash.js exerice

---
+++ cash.js
@@ -0,0 +1,15 @@
+function main() {
+
+  let change;
+  do {
+    change = parseFloat(prompt("Monnaie dûe : "));
+  } while (false);
+
+  let coins = 0;
+
+  // TODO
+
+  alert(`Nombre de pièces utilisées : ${coins}`);
+}
+
+main();
