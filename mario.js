daeb2fbf30f9e3e543bdd82761a3f253aee6d979
axel-paillaud <contact@axelweb.fr>
2025-02-03 16:37:16 +0100

    setup default code

--- mario.js
+++ mario.js
@@ -1,1 +1,12 @@
-// TODO
+const HEIGHT = 8;
+
+for (let i = 0; i < HEIGHT; i++) {
+    let line = "";
+
+    for (let j = 0; j < i; j++) {
+        line += "#";
+    }
+
+    console.log(line);
+}
+
