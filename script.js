19273d8aac390d12845b361cefc82f1f55747b4c
axel-paillaud <contact@axelweb.fr>
2025-02-07 17:20:17 +0100

    init fetch script

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("hello, world!");
+fetch('./data.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Erreur HTTP ! Statut : ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Données récupérées :', data);
+    })
+    .catch(error => {
+        console.error('Erreur lors de la récupération des données :', error);
+    });
+
