14dad074b817950ee64f7ed1d36997fdcef7eb6b
axel-paillaud <contact@axelweb.fr>
2025-02-07 17:46:22 +0100

    end dom example

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-fetch('./data.json')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`Erreur HTTP ! Statut : ${response.status}`);
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log('Données récupérées :', data);
-    })
-    .catch(error => {
-        console.error('Erreur lors de la récupération des données :', error);
-    });
+function fetchData(key) {
+    fetch('./data.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Erreur HTTP ! Statut : ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            const filteredResults = data.students.filter(person => 
+                person.first_name.toLowerCase().startsWith(key) || 
+                person.last_name.toLowerCase().startsWith(key)
+            );
+            console.log('Résultats filtrés :', filteredResults);
+        })
+        .catch(error => {
+            console.error('Erreur lors de la récupération des données :', error);
+        });
+}
+
+document.addEventListener('keydown', event => {
+    console.log(`Touche appuyée : ${event.key}`);
+    fetchData(event.key);
+});
+
