1dff5e3b662a83773cbef369d901e6416879325b
axel-paillaud <contact@axelweb.fr>
2025-02-07 17:33:26 +0100

    add fetch js script

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-// TODO Récupérer la liste des étudiants,
-// l'afficher dans la console
+fetch('./data.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Erreur HTTP ! Statut : ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Données récupérées :', data);
+    })
+    .catch(error => {
+        console.error('Erreur lors de la récupération des données :', error);
+    });
