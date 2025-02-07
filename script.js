fa81cf1653d8343c2fcb143cca0adf2b4dbab864
axel-paillaud <contact@axelweb.fr>
2025-02-07 17:32:50 +0100

    remove script

--- script.js
+++ script.js
@@ -1,14 +1,2 @@
-fetch('./data.json')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`Erreur HTTP ! Statut : ${response.status}`);
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log('Données récupérées :', data);
-    })
-    .catch(error => {
-        console.error('Erreur lors de la récupération des données :', error);
-    });
-
+// TODO Récupérer la liste des étudiants,
+// l'afficher dans la console
