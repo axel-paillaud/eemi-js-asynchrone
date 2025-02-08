b369af5dcb644a666733c06a96c9cbf947d088b7
axel-paillaud <contact@axelweb.fr>
2025-02-08 09:42:51 +0100

    show list on DOM based on keypress

--- script.js
+++ script.js
@@ -12,10 +12,29 @@ function fetchData(key) {
                 person.last_name.toLowerCase().startsWith(key)
             );
             console.log('Résultats filtrés :', filteredResults);
+
+            updateList(filteredResults);
         })
         .catch(error => {
             console.error('Erreur lors de la récupération des données :', error);
         });
+}
+
+
+function updateList(students) {
+    const resultList = document.getElementById('result-list');
+    resultList.innerHTML = ''; // Vider la liste avant de la mettre à jour
+    
+    if (students.length === 0) {
+        resultList.innerHTML = '<li>Aucun résultat trouvé</li>';
+        return;
+    }
+    
+    students.forEach(student => {
+        const li = document.createElement('li');
+        li.innerHTML = `<img src="${student.image}" alt="${student.first_name} ${student.last_name}" width="30"> <strong>${student.first_name} ${student.last_name}</strong> - ${student.specialty}`;
+        resultList.appendChild(li);
+    });
 }
 
 document.addEventListener('keydown', event => {
