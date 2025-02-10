563820e4d65f2aca3b6783cee0386088ab8b0b77
Axel Paillaud <contact@axelweb.fr>
2025-02-10 05:44:34 +0100

    add solution to mario exercise

--- mario.js
+++ mario.js
@@ -1,1 +1,24 @@
-// TODO
+function drawPyramid(n) {
+    if (n <= 0 || isNaN(n)) {
+        console.log("Veuillez entrer un nombre valide !");
+        return;
+    }
+
+    for (let i = 1; i <= n; i++) {
+        let line = "";
+        // Loop  to add space
+        for (let y = 0; y < n - i; y++) {
+            line += " ";
+        }
+        // Loop to add blocks
+        for (let y = 0; y < i; y++) {
+            line += "#";
+        }
+        console.log(line);
+    }
+}
+
+const height = 12;
+
+drawPyramid(height);
+
